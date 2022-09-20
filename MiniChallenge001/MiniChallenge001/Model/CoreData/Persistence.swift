cf707d304712459fdb7aacaa205bb8d76c002c5c
Levenetz <93161067+Levenetz@users.noreply.github.com>
2022-09-20 17:56:43 -0300

    adasdasd

--- MiniChallenge001/MiniChallenge001/Model/CoreData/Persistence.swift
+++ MiniChallenge001/MiniChallenge001/Model/CoreData/Persistence.swift
@@ -18,8 +18,7 @@ struct PersistenceController {
         
         // cria automaticamente 10 exemplos de itens na lista
         for _ in 0..<10 {
-            let newItem = Arma(context: viewContext)
-            newItem.nome = ""
+            
         }
         do {
             try viewContext.save()
