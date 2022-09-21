fe1315c6dcfc5a03727096a1016966eaf46476d8
oManhattan <matheusbatuque@gmail.com>
2022-09-21 09:40:33 -0300

    objetos json para decodar e encodar

---
+++ MiniChallenge001/MiniChallenge001/Model/Json/ArmaJSON.swift
@@ -0,0 +1,41 @@
+//
+//  ArmaJSON.swift
+//  MiniChallenge001
+//
+//  Created by Matheus Cavalcanti de Arruda on 15/09/22.
+//
+
+import Foundation
+
+enum TipoDano: String, Codable {
+    case perfurante = "Perfurante"
+    case cortante = "Cortante"
+    case concussao = "Concussão"
+    case none = "Nenhum"
+}
+
+enum EstiloArma: String, Codable {
+    case distancia = "Distância"
+    case cac = "Corpo a Corpo"
+}
+
+enum TipoArma: String, Codable {
+    case simples = "Simples"
+    case marcial = "Marcial"
+}
+
+struct Dano: Codable {
+    var dano: String
+    var tipo: TipoDano
+}
+
+struct ArmaJSON: Item {
+    var id: Int
+    var nome: String
+    var tipo: TipoArma
+    var estilo: EstiloArma
+    var preco: Moeda
+    var dano: Dano
+    var peso: Float
+    var propriedades: String
+}
--- MiniChallenge001/MiniChallenge001/Model/JsonFileUtil.swift
+++ MiniChallenge001/MiniChallenge001/Model/JsonFileUtil.swift
@@ -12,11 +12,24 @@ class JsonFileUtil {
     
     private static let STANDARD_PATH = "Content"
     
-    public static func getFilesNameFromFolder(folder path: String) -> [URL] {
+    public static func getFilesURLFromFolder(folder path: String) -> [URL] {
         let folderPath = "\(STANDARD_PATH)/\(path)"
         guard let filesURL = Bundle.main.urls(forResourcesWithExtension: "json", subdirectory: folderPath) else { return [] }
         return filesURL
     }
     
-    
+    public static func getDataFromFiles<T:Item>(folder name: String, decoder: T.Type) -> [Item] {
+        let paths = getFilesURLFromFolder(folder: name)
+        var items: [Item] = []
+        for path in paths {
+            do {
+                let data = try Data(contentsOf: path)
+                let decodedData = try JSONDecoder().decode(T.self, from: data)
+                items.append(decodedData)
+            } catch {
+                print("Error on path: \(path)")
+            }
+        }
+        return items
+    }
 }
